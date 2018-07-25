8d442f7c441cb24a18ea5ff8e6bf37bc56f91f57
Michael Lee <michael.lee@slickdeals.net>
2018-07-25 15:30:13 -0700

    SimpleQueue

--- Swift Data Structures/Swift Data Structures/Stacks and Queues/SimpleQueue.swift
+++ Swift Data Structures/Swift Data Structures/Stacks and Queues/SimpleQueue.swift
@@ -1,45 +1,52 @@
 //
-//  SimpleStack.swift
+//  SimpleQueue.swift
 //  Swift Data Structures
 //
-//  Created by Michael Lee on 6/14/18.
+//  Created by Michael Lee on 7/25/18.
 //  Copyright © 2018 Michael Lee. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
-// Generic Stack class for any type. Implemented
+// Generic Queue class for any type. Implemented
 // with a swift array backing store
 //
-// The Top of the stack is the last item in the array
-// Push & Pop from the end of the array
+// Push to the Head of the Queue. Pop from the Tail
+//
+// Head is the last item in the backing store.
+// Tail is the first item in the backing store.
 
 // All operations are Order(1) - on backingStore
 
-public class SimpleStack<Element> {
+public class SimpleQueue<Element> {
     
     private var backingStore: [Element]
     
     public var count: Int { return backingStore.count }
     
-    public var topElement: Element? {
+    public var headElement: Element? {
         guard backingStore.count > 0 else { return nil }
         return backingStore.last
     }
     
-    public init(with array: [Element]) { backingStore = array }
+    public var tailElement: Element? {
+        guard backingStore.count > 0 else { return nil }
+        return backingStore.first
+    }
 
+    public init(with array: [Element]) { backingStore = array }
+    
     public convenience init() { self.init(with: [Element]() ) }
     
     public func push(_ item: Element) { backingStore.append(item) }
     
     public func pop() -> Element? {
         guard backingStore.count > 0 else { return nil }
-        let item = backingStore.last
-        backingStore.removeLast()
+        let item = backingStore.first
+        backingStore.removeFirst()
         return item
     }
     
     public func asArray() -> [Element] { return backingStore }
-    
+
 }
